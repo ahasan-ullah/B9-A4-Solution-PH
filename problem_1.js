6f5db5b30187b25ec803310234ef4584e351ddf5
Ahasan Ullah <ahasanullah451@gmail.com>
2024-09-14 00:51:21 +0600

    problem 1 solved

---
+++ problem_1.js
@@ -0,0 +1,11 @@
+function calculateMoney(ticketSale){
+  if(ticketSale<0 || typeof ticketSale!=="number"){
+    return "Invalid Number";
+  }
+  return ticketSale*120-(500+8*50);
+}
+
+console.log(calculateMoney(10));
+console.log(calculateMoney(1055));
+console.log(calculateMoney(93));
+console.log(calculateMoney(-130));
